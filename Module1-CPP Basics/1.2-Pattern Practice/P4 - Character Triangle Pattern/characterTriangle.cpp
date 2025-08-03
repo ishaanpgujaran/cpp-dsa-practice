0f06376a426fb000ce23ca76b7ed3cebf3e4eb9b
ishaanpgujaran <ishaangujaran@gmail.com>
2025-08-03 08:42:18 +0530

    Day 1 : Module 1 (Video 4 - Patterns) - P4 : Character Triangle Pattern

--- Module1-CPP Basics/1.2-Pattern Practice/P4 - Character Triangle Pattern/characterTriangle.cpp
+++ Module1-CPP Basics/1.2-Pattern Practice/P4 - Character Triangle Pattern/characterTriangle.cpp
@@ -4,16 +4,22 @@ using namespace std ;
 
 int main () {
     int n = 9 ;
-    cout << "\n** Pattern 3 - Number Triangle Pattern **\n" << endl ;
+    cout << "\n** Pattern 4 - Character Triangle Pattern **\n" << endl ;
+
+    // Define ch Starting Character Variable 
+    char ch = 'A' ; 
     
     // Outer Loop for Each Row - n Rows will Print
     for (int i = 0; i < n; i++) {
         
-        //Inner Loop for Each Character - i+1 Characters will Print
+        // Inner Loop for Each Character - i+1 Characters will Printin each Row
         for (int j=0; j < i+1; j++) {
-            //Print Character - i + 1
-            cout << (i + 1) << " ";
+            // Print Character
+            cout << ch << " ";
         }
+
+        // Increment for Next Character
+        ch++ ;
         
         // Move to Next Line
         cout << endl ;
