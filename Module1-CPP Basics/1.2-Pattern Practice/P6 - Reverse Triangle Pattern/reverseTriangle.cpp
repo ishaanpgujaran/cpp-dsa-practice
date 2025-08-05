e7c68c0e247c1e473b080622be56d721805b5115
ishaanpgujaran <ishaangujaran@gmail.com>
2025-08-05 07:02:49 +0530

    Day 3 : Module 1- C++ Basics (Video 4 - Patterns) - P6 : Reverse Triangle Pattern

---
+++ Module1-CPP Basics/1.2-Pattern Practice/P6 - Reverse Triangle Pattern/reverseTriangle.cpp
@@ -0,0 +1,27 @@
+// Pattern 6 - Reverse Triangle Pattern
+#include<iostream>
+using namespace std ;
+
+int main() {
+    cout << "\n** Pattern 6 - Reverse Triangle Pattern **\n" << endl;
+
+    // Define the Number of Lines to Print
+    int n = 9 ; 
+
+    // Outer Loop : Decides How many Lines - Run n Times
+    for(int i = 0 ; i < n ; i++) {
+        
+        // Inner Loop : Decides How many Characters in Each Line & What to Print
+        // We will Print i+1 times - Run i+1 times 
+        // As Reverse Order, We will Reverse the Condition : Run from i+1 to 1 & print j
+        for(int j = i+1 ; j > 0 ; j--) {
+            // Print j for Character in each line
+            cout << j << " " ;
+        }
+
+        // Move to Next Line
+        cout << endl ;
+    }
+
+    return 0 ;
+}
